319ea3fa2b670ee40c6d1534e60c3eecf1091628
zvezd1n <igorzvezdin2021@gmail.com>
2022-06-25 17:16:41 +0300

    two commit

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,19 @@
 $(function(){
     
 });
+
+let scrollpos = window.scrollY;
+
+const header = document.querySelector(".header");
+const scrollChange = 3;
+
+const add_class_on_scroll = () => header.classList.add("header--bg-white");
+const remove_class_on_scroll = () => header.classList.remove("header--bg-white");
+
+window.addEventListener('scroll', function() { 
+  scrollpos = window.scrollY;
+
+  if (scrollpos >= scrollChange) { add_class_on_scroll() }
+  else { remove_class_on_scroll() }
+  
+})
